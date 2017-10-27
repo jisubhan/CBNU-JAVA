de70c85b735d4fd2893ef0a250d5fc842083ab5f
HanJiSub <jisub3054@gmail.com>
2017-10-27 16:14:22 +0900

    10.27

---
+++ 10.27/src/practice/ExcptionExist.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class ExcptionExist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		func();
+	}
+	
+	static void func()
+	{
+
+		int i = 0;
+		int j =1;
+		
+		System.out.println(i/j);
+	
+
+}
+}
---
+++ 10.27/src/practice/StaticImport.java
@@ -0,0 +1,13 @@
+package practice;
+
+import static java.lang.System.out;
+import static java.lang.Thread.MAX_PRIORITY;
+public class StaticImport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println("Hello");
+		out.println(MAX_PRIORITY);
+	}
+
+}
